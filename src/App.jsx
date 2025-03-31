c2a28491e9bc3647d525e33730b31f89c80733cf
Said Yolciyev <said14y2007@gmail.com>
2025-03-31 16:36:04 +0400

    1.11

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { WatchList } from "./pages/WatchList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MovieDetails } from "./pages/MovieDetails";
 
-export const API = "http://www.omdbapi.com/?apikey=d47c1c7c&";
+export const API = "https://www.omdbapi.com/?apikey=d47c1c7c&";
 
 const App = () => {
   const [search, setSearch] = useState("interstellar");
